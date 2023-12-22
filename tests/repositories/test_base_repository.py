2df9f2d72dde088fd9ef584d714ecaec946fa9a8
Дмитро Рєзєнков <108422398+RezenkovD@users.noreply.github.com>
2023-12-22 22:00:46 +0200

    Feat: add unittests (#3)
    * req: add Faker to req-dev
    
    * feat: add User factory
    
    * test: add test for BaseRepository
    
    * ci: add continuous integration
    
    * fix: correct import module
    
    * req: add flake8 to req-dev

---
+++ tests/repositories/test_base_repository.py
@@ -0,0 +1,115 @@
+import unittest
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from database import UnitOfWork
+from models import User
+from repositories.impl import BaseRepository
+from enums import UserStatus
+
+
+class TestBaseRepository(unittest.TestCase):
+    def setUp(self):
+        engine = create_engine("sqlite:///:memory:")
+        Session = sessionmaker(bind=engine)
+        self.session = Session()
+
+        User.metadata.create_all(bind=engine)
+
+        self.unit_of_work = UnitOfWork(lambda: self.session)
+
+    def tearDown(self):
+        User.metadata.drop_all(bind=self.session.bind)
+
+    def test_base_repository_get_all(self):
+        with self.unit_of_work as uow:
+            user_repository = BaseRepository(uow._session, User)
+
+            users = user_repository.get_all()
+            self.assertEqual(len(users), 0)
+
+            user = User(
+                first_name="John",
+                last_name="Doe",
+                password="secret",
+                role=UserStatus.DRIVER,
+            )
+            uow.users.create(user)
+
+            users = user_repository.get_all()
+            self.assertEqual(len(users), 1)
+
+    def test_base_repository_get_by_id(self):
+        with self.unit_of_work as uow:
+            user_repository = BaseRepository(uow._session, User)
+
+            non_existing_user = user_repository.get_by_id(1)
+            self.assertIsNone(non_existing_user)
+
+            user = User(
+                first_name="John",
+                last_name="Doe",
+                password="secret",
+                role=UserStatus.DRIVER,
+            )
+            uow.users.create(user)
+
+            retrieved_user = user_repository.get_by_id(user.id)
+            self.assertIsNotNone(retrieved_user)
+            self.assertEqual(retrieved_user.first_name, "John")
+
+    def test_base_repository_create(self):
+        with self.unit_of_work as uow:
+            user_repository = BaseRepository(uow._session, User)
+
+            user = User(
+                first_name="John",
+                last_name="Doe",
+                password="secret",
+                role=UserStatus.DRIVER,
+            )
+
+            user_repository.create(user)
+
+            self.assertIsNotNone(user.id)
+
+    def test_base_repository_update(self):
+        with self.unit_of_work as uow:
+            user_repository = BaseRepository(uow._session, User)
+
+            user = User(
+                first_name="John",
+                last_name="Doe",
+                password="secret",
+                role=UserStatus.DRIVER,
+            )
+            uow.users.create(user)
+
+            user.first_name = "Updated Name"
+            user_repository.update(user)
+
+            updated_user = uow.users.get_by_id(user.id)
+            self.assertIsNotNone(updated_user)
+            self.assertEqual(updated_user.first_name, "Updated Name")
+
+    def test_base_repository_delete(self):
+        with self.unit_of_work as uow:
+            user_repository = BaseRepository(uow._session, User)
+
+            user = User(
+                first_name="John",
+                last_name="Doe",
+                password="secret",
+                role=UserStatus.DRIVER,
+            )
+            uow.users.create(user)
+
+            result = user_repository.delete(user.id)
+            self.assertTrue(result)
+
+            deleted_user = uow.users.get_by_id(user.id)
+            self.assertIsNone(deleted_user)
+
+
+if __name__ == "__main__":
+    unittest.main()
